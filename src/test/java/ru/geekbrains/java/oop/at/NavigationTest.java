5638aac0fa6d00a39a4e9e2ad750b4cd93b41422
Vitaly Zverev <vzhka@post.cz>
2020-07-28 07:39:34 +0300

    В проверку Навигации добален параметрезированный тест

--- src/test/java/ru/geekbrains/java/oop/at/NavigationTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/NavigationTest.java
@@ -1,10 +1,9 @@
 package ru.geekbrains.java.oop.at;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
 import ru.geekbrains.java.oop.at.base.BaseTestNavigation;
 
 
@@ -23,91 +22,26 @@ public class NavigationTest extends BaseTestNavigation {
         - Карьера
     */
 
-    @DisplayName("Проверка загрузки страницы Вебинары")
-    @Test
-    public void events() {
+    @CsvSource({
+            "'Вебинары', 'events'",
+            "'Курсы', 'courses'",
+            "'Форум', 'topics'",
+            "'Блог', 'posts'",
+            "'Тесты', 'tests'",
+            "'Карьера', 'career'"
+    })
 
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-webinar\"]"))
+
+    @DisplayName("Проверка загрузки страниц")
+    @ParameterizedTest
+    public void allTests(String name, String url) {
+
+        driver.findElement(By.cssSelector("[class=\"gb-left-menu__nav\"] [href='/" + url + "']"))
                 .click();
         Assertions.assertEquals(
-                "Вебинары",
+                name,
                 driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
         );
 
     }
-
-    @DisplayName("Проверка загрузки страницы Курсы")
-    @Test
-    public void courses() {
-
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-courses\"]"))
-                .click();
-        Assertions.assertEquals(
-                "Курсы",
-                driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
-        );
-
-        //chromeDriver.findElementByCssSelector("[class=\"gb-empopup-close\"]").click();
-        driver.findElement(By.cssSelector("button [class=\"svg-icon icon-popup-close-button \"]"))
-                .click();
-
-    }
-
-    @DisplayName("Проверка загрузки страницы Форум")
-    @Test
-    public void forum() {
-
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-forum\"]"))
-                .click();
-        Assertions.assertEquals(
-                "Форум",
-                driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
-        );
-
-    }
-
-    @DisplayName("Проверка загрузки страницы Блоги")
-    @Test
-    public void blogs() {
-
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-blog\"]"))
-                .click();
-        Assertions.assertEquals(
-                "Блог",
-                driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
-        );
-
-        driver.findElement(By.cssSelector("[class=\"gb-empopup-close\"]"))
-                .click();
-        driver.findElement(By.cssSelector("button [class=\"svg-icon icon-popup-close-button \"]")).click();
-
-    }
-
-    @DisplayName("Проверка загрузки страницы Тесты")
-    @Test
-    public void tests() {
-
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-tests\"]"))
-                .click();
-
-        Assertions.assertEquals(
-                "Тесты",
-                driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
-        );
-
-    }
-
-    @DisplayName("Проверка загрузки страницы Карьера")
-    @Test
-    public void career() {
-
-        driver.findElement(By.cssSelector("[class=\"svg-icon icon-career\"]"))
-                .click();
-        Assertions.assertEquals(
-                "Карьера",
-                driver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
-        );
-
-    }
-
 }
