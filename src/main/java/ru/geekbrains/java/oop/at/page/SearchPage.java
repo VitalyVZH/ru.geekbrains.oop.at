602bcdc38c43bb47074823ffe94c07621b6c053e
Vitaly Zverev <vzhka@post.cz>
2020-08-01 23:46:50 +0300

    Изменена проверка количества контента на странице Курсы

--- src/main/java/ru/geekbrains/java/oop/at/page/SearchPage.java
+++ src/main/java/ru/geekbrains/java/oop/at/page/SearchPage.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.java.oop.at.page;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -19,26 +20,38 @@ public class SearchPage extends PageFactory {
     @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='professions']/span")
     private WebElement buttonProfession;
 
-    /*@FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;
+    @FindBy(css = "[id=\"nav\"] [href=\"/courses\"]")
+    private WebElement buttonCourses;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='professions']/span")
+    private WebElement countProfessions;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='courses']/span")
+    private WebElement countCourses;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='webinars']/span")
+    private WebElement countWebinars;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='blogs']/span")
+    private WebElement countBlogs;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='forums']/span")
+    private WebElement countForums;
+
+    @FindBy(xpath = "//a[@class='search-page-tabs__item' and @data-tab='tests']/span")
+    private WebElement countTests;
+
+    @FindBy(css = "[placeholder=\"Email\"]")
+    private WebElement inputLogin;
 
     @FindBy(css = "[placeholder=\"Пароль\"]")
     private WebElement inputPassword;
 
-    @FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;
+    @FindBy(id = "registration-form-button")
+    private WebElement buttonSighIn;
 
-    @FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;
-
-    @FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;
-
-    @FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;
-
-    @FindBy(css = "[placeholder=\"Пароль\"]")
-    private WebElement inputPassword;*/
+    @FindBy(css = "[id=\"top-menu\"] h2")
+    private WebElement namePage;
 
 
     // отключение popUp
@@ -48,11 +61,28 @@ public class SearchPage extends PageFactory {
     @FindBy(css = "button [class=\"svg-icon icon-popup-close-button \"]")
     private WebElement buttonClosePopUp2;
 
+    public SearchPage authorization(String login, String password) {
+
+        inputLogin.sendKeys(login);
+        inputPassword.sendKeys(password);
+        buttonSighIn.click();
+
+        return new SearchPage(driver);
+    }
+
     private WebDriver driver;
 
     public SearchPage (WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
+    }
+
+    public void checkNamePage(String expectedNamePage) {
+        Assertions.assertEquals(expectedNamePage, namePage.getText());
+    }
+
+    public void openUrl() {
+        driver.get("https://geekbrains.ru/");
     }
 
 
@@ -64,5 +94,29 @@ public class SearchPage extends PageFactory {
 
     public WebElement getButtonClosePopUp2() { return buttonClosePopUp2; }
 
-    public WebElement getButtonProfession() { return buttonProfession; }
+    public WebElement getButtonCourses() { return buttonCourses; }
+
+    public WebElement getCountProfessions() { return countProfessions; }
+
+    public WebElement getCountCourses() { return countCourses; }
+
+    public WebElement getCountWebinars() { return countWebinars; }
+
+    public WebElement getCountBlogs() { return countBlogs; }
+
+    public WebElement getCountForums() { return countForums; }
+
+    public WebElement getCountTests() { return countTests; }
+
+    public WebElement getInputLogin() {
+        return inputLogin;
+    }
+
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
+
+    public WebElement getButtonSighIn() {
+        return buttonSighIn;
+    }
 }
--- src/test/java/ru/geekbrains/java/oop/at/SearchWebTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/SearchWebTest.java
@@ -1,19 +1,16 @@
 package ru.geekbrains.java.oop.at;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.geekbrains.java.oop.at.base.BaseWebTest;
+import ru.geekbrains.java.oop.at.page.ContentPage;
 import ru.geekbrains.java.oop.at.page.SearchPage;
 
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@DisplayName("Поисковый тест")
+@DisplayName("Поиск")
 public class SearchWebTest extends BaseWebTest {
 
     /*
@@ -31,131 +28,71 @@ public class SearchWebTest extends BaseWebTest {
     В Проектах и компаниях отображается GeekBrains
     */
 
-    @BeforeEach
-    void beforeEach() {
+
+    @DisplayName("Проверка количества контента")
+    @Test
+    void searchTest() {
 
         SearchPage searchPage = new SearchPage(driver);
 
-        //      Перейти на сайт https://geekbrains.ru/courses
+        new SearchPage(driver)
+                .openUrl();
 
-        driver.get("https://geekbrains.ru/courses");
+        //      Авторизация на сайте
+
+        //driver.get("https://geekbrains.ru/");
+
+        String login = "hks47018@eoopy.com";
+        String password = "hks47018";
+
+        new SearchPage(driver)
+                .authorization(login, password)
+                .checkNamePage("Главная");
+
+        // Перейти на сайт https://geekbrains.ru/courses
+
+        wait15second = new WebDriverWait(driver, 15);
+        searchPage.getButtonCourses().click();
+
+        new ContentPage(driver)
+                .checkNamePage("Курсы");
 
 //        searchPage.getButtonClosePopUp1().click();
-        searchPage.getButtonClosePopUp2().click();
+//        searchPage.getButtonClosePopUp2().click();
 
 //      В поле Поиск ввести текст: java
 
         searchPage.getSearchBar().click();
         searchPage.getSearchQuery().sendKeys("java");
 
-        wait15second = new WebDriverWait(driver, 15);
+        wait15second = new WebDriverWait(driver, 25);
 
-    }
+//        Профессий не менее чем 2
 
-    @DisplayName("Проверка количества профессий")
-    @Test
-    void professions() {
+       assertThat(Integer.parseInt(searchPage.getCountProfessions().getText()), greaterThanOrEqualTo(2));
 
-//      Проверить что на странице: Профессий не менее чем 2
+//        Курсов более 15
 
-        assertThat(Integer.parseInt(driver.findElement
-                (By.xpath("//a[@class='search-page-tabs__item' " +
-                        "and @data-tab='professions']/span"))
-                        .getText())
-                , greaterThanOrEqualTo(2));
-
-    }
-
-    @DisplayName("Проверка количества курсов")
-    @Test
-    public void courses() {
-
-//      Курсов более 15
-
-        assertThat(Integer.parseInt(driver.findElement
-               (By.xpath("//a[@class='search-page-tabs__item' " +
-                       "and @data-tab='courses']/span"))
-               .getText()), greaterThan(15));
-
-    }
-
-    @DisplayName("Проверка количества вебинаров")
-    @Test
-    public void webinars() {
+        assertThat(Integer.parseInt(searchPage.getCountCourses().getText()), greaterThan(15));
 
 //      Вебинаров больше чем 180, но меньше 300
 
-        assertThat(Integer.parseInt(driver.findElement
-                (By.xpath("//a[@class='search-page-tabs__item' " +
-                        "and @data-tab='webinars']/span")).getText()), allOf(
+        assertThat(Integer.parseInt(searchPage.getCountWebinars().getText()), allOf(
                 greaterThan(180),
-                lessThan(300))
-        );
-    }
-
-    @DisplayName("Проверка наличия и порядкового номера раздела")
-    @Test
-    public void juniors() {
-
-//  В вебинарах отображается первым “Java Junior. Что нужно знать для успешного собеседования?”
-
-        WebElement junior = driver
-                .findElement(By.xpath("(//a[@href='/events/6'])[2]"));
-
-        wait15second.until(ExpectedConditions.visibilityOf(junior));
-
-        assertThat(junior.getText()
-                 , equalTo("Java Junior. Что нужно знать для успешного собеседования?"));
-
-    }
-
-    @DisplayName("Проверка количества блогов")
-    @Test
-    public void blogs() {
+                lessThan(300)
+        ));
 
 //        Блогов более 300
 
-        assertThat(Integer.parseInt(driver
-                .findElement(By.xpath("//a[@class='search-page-tabs__item' " +
-                        "and @data-tab='blogs']/span")).getText())
-                , greaterThan(300));
-    }
-
-    @DisplayName("Проверка количества форумов")
-    @Test
-    public void forums() {
+        assertThat(Integer.parseInt(searchPage.getCountBlogs().getText()), greaterThan(300));
 
 //        Форумов не 350
 
-        assertThat(Integer.parseInt(driver
-                .findElement(By.xpath("//a[@class='search-page-tabs__item' " +
-                        "and @data-tab='forums']/span")).getText())
-                , not(equalTo(350)));
-    }
-    @DisplayName("Проверка количества тестов")
-    @Test
-    public void tests() {
+        assertThat(Integer.parseInt(searchPage.getCountForums().getText()), not(equalTo(350)));
 
 //        Тестов не 0
 
-        assertThat(Integer.parseInt(driver
-                        .findElement(By.xpath("//a[@class='search-page-tabs__item' " +
-                                "and @data-tab='tests']/span")).getText())
-                , not(equalTo(0)));
-    }
+        assertThat(Integer.parseInt(searchPage.getCountTests().getText()), not(equalTo(0)));
 
-    @DisplayName("Проверка раздела Проекты и компании")
-    @Test
-    public void geekbrains() {
-
-//        В Проектах и компаниях отображается GeekBrains
-
-        WebElement gb = driver
-                .findElement(By.xpath("//a[contains(text(), 'GeekBrains')]"));
-
-        wait15second.until(ExpectedConditions.visibilityOf(gb));
-
-        assertThat(gb.getText()
-                , containsString ("GeekBrains"));
     }
 }
